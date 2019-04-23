fb197317f86c862911c97bd3cc0719668028ff31
Elkan Frank <efrank@student.codam.nl>
2019-04-23 14:54:56 +0200

    centre black

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: efrank <efrank@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/22 15:13:36 by efrank         #+#    #+#                */
-/*   Updated: 2019/04/23 12:41:47 by efrank        ########   odam.nl         */
+/*   Updated: 2019/04/23 13:52:57 by efrank        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,19 @@ static t_color	set_color(int n)
 {
 	t_color color;
 
-	color.red = ft_map((double)n, 0, MAX_ITER, 0x10, 0x0F);
-	color.green = ft_map((double)n, 0, MAX_ITER, 0x10, 0x71);
-	color.blue = ft_map((double)n, 0, MAX_ITER, 0x10, 0x73);
+	color.red = ft_map((double)n, 0, MAX_ITER, 0x10, 0x23);
+	color.green = ft_map((double)n, 0, MAX_ITER, 0x10, 0x80);
+	color.blue = ft_map((double)n, 0, MAX_ITER, 0x10, 0xF4);
+	return (color);
+}
+
+static t_color max_color(void)
+{
+	t_color color;
+
+	color.red = 0x10;
+	color.green = 0x10;
+	color.blue = 0x10;
 	return (color);
 }
 
@@ -46,9 +56,11 @@ t_color			mandelbrot(t_mlx mlx, int x, int y)
 		fractal.b_new = 2 * fractal.a * fractal.b;
 		fractal.a = fractal.a_new + fractal.ca;
 		fractal.b = fractal.b_new + fractal.cb;
-		if (fabs(fractal.a + fractal.b) > 20)
+		if (fabs(fractal.a + fractal.b) > 16)
 			break ;
 		n++;
 	}
+	if (n == MAX_ITER)
+		return (max_color());
 	return (set_color(n));
 }
